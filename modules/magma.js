ea1e5423f862f2b88eba03c2830a02b753f5f10f
ryfeng <ryfeng@gmail.com>
2017-05-12 01:18:59 -0400

    fix overclock

--- modules/magma.js
+++ modules/magma.js
@@ -167,14 +167,16 @@ function autoGenerator2() {
   const mode = getPageSetting('AutoGen2'); // None : Microtick : Cap : Overclock
   if (!mode) // Default: move on
     return;
+  else if (mode == 3) {
+	changeGeneratorState(FUEL);
+	return;
+  }
 
   const fuel = game.global.magmaFuel;
   const want = mode == 1 ? getFuelBurnRate() : getGeneratorFuelCap();
-  const overclock = mode == 3;
+  
   if (!game.global.generatorMode) { // Currently: Gain Mi
-    if (overclock)
-	  changeGeneratorState(FUEL);
-    else if (fuel < want)
+    if (fuel < want)
       changeGeneratorState(game.permanentGeneratorUpgrades.Hybridization.owned ? HYBRID : FUEL);
   } else if (fuel >= want) // Not gaining Mi when we should
     changeGeneratorState(MI);
